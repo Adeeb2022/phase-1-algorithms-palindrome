6dfd9d11451a9d1e281f7c9bb239e0214c0d0a0c
Adeeb2022 <adeebsamad01@gmail.com>
2023-02-23 14:37:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const letterArray = word.split('');
+  const arrayJoin = letterArray.reverse().join('');
+  if (word === arrayJoin) {
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
 */
-
+// * Spliting word to an array of letters
+// * Reversing the array of letters
+// * Joining letters after reversing them
+// * Comparing the new words to the original one
 /*
   Add written explanation of your solution here
 */
-
+isPalindrome('word')
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
